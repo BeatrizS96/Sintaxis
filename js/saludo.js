d80afc8d54396c8c515f4dc1c07d01d348916cea
Beatriz <beatriz.sn.8@gmail.com>
2019-10-14 17:23:09 +0200

    Primer commit

---
+++ js/saludo.js
@@ -0,0 +1,7 @@
+alert("Hola mundo");
+//confirm("Mensaje con confirmación");
+//prompt("¿Cuál es tu nombre?");
+document.write("Usando método write");
+console.log("Mensaje de información");
+console.warn("Mensaje de advertencia");
+console.error("Mensaje de error");
