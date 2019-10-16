52e2560e8af9bd75727204ae90dc9277d9e90251
Beatriz <beatriz.sn.8@gmail.com>
2019-10-16 20:59:21 +0200

    Ejercicio 5.1

--- js/saludo.js
+++ js/saludo.js
@@ -6,3 +6,4 @@ document.write("Usando método write");
 console.log("Mensaje de información");
 console.warn("Mensaje de advertencia");
 console.error("Mensaje de error");
+document.getElementById("celda1").innerHTML="Estado Unidos";
